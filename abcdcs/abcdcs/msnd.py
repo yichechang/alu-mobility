f03446fc2cc5d2c106e14344c5eb05cc717167d1
Yi-Che Oliver Chang <yichechang@gmail.com>
2023-03-31 00:46:25 -0400

    remove unused function in msnd

--- abcdcs/abcdcs/msnd.py
+++ abcdcs/abcdcs/msnd.py
@@ -9,65 +9,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-# def calculate_msnd(ds, components=['u', 'v'], pixsize=None
-# ) -> Tuple[pd.DataFrame, pd.DataFrame]:
-#     """MSND analysis from PIV displacement fields.
-
-#     Compute MSND for each lag time with all possible pairs, but outliers
-#     (absolute value of zscore >= 3) excluded.
-
-#     Parameters
-#     ----------
-#     ds : xarray.Dataset
-#         Displacement fields at different lag times from a single 
-#         movie
-#     components : list, optional
-#         Names of displacement field components, by default ['u','v']
-#     pixsize : float, optional
-#         Micron per pixel (px). Default to None will use 1 for calculation.
-#         If "meta" is provided, will use the unit information from `ds`
-#         (`ds.attrs["pixelsize"]`).
-
-#     Returns
-#     -------
-#     stats : pd.DataFrame
-#         Dataframe with lag time and corresponding mean and SD of MSND across
-#         all pairs sampled.
-#     raw : pd.DataFrame
-#         Dataframe with MSND for individual PIV displacement fields.
-#     """
-#     # Don't alter the Dataset passed in as argument
-#     piv = ds.copy()
-
-#     # x and y components for the displacement field
-#     u, v = components
-#     # convert pixels to microns?
-#     if pixsize is None:
-#         pixsize = 1
-#     elif pixsize == "meta":
-#         pixsize = ds.attrs["pixelsize"]
-
-#     # MSND for each displacement field
-#     piv['snd'] = pixsize**2 * (piv[u]**2 + piv[v]**2)
-#     piv['msnd'] = piv['snd'].mean(dim=('X','Y'), skipna=True)
-#     df = (piv[["msnd"]]
-#           .to_dataframe()
-#           .dropna(axis=0, how='any').reset_index()
-#     )
-#     df = df[["lag", "lag_s", "T", "T_s", "msnd"]]
-
-#     # Remove outliers before aggregating all MSND for the same lag time
-#     zscore = lambda x: (x - x.mean()) / x.std()
-#     z_thresh = 3
-#     df['zscore'] = df.groupby('lag')['msnd'].transform(zscore)
-#     stats = (df[df['zscore'].abs() < z_thresh]
-#         .groupby('lag_s')['msnd']
-#         .aggregate(msnd_mean='mean', msnd_std='std')
-#         .reset_index()
-#     )
-    
-#     return stats, df
-
 def calculate_msnd(
         ds, 
         components=['u', 'v'], 
