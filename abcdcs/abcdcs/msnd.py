76bff8233dcb21fd7085725f3a4ac146b2fbf4c0
Yi-Che Oliver Chang <yichechang@gmail.com>
2023-05-14 22:43:21 -0400

    msnd (#17)
    msnd calculation from PIV fields directly, supporting 'normal', 'eachlevel', and 'weighted' methods. 
    
    * Rewrote MSND calculation
    * fix typo in docstring

--- abcdcs/abcdcs/msnd.py
+++ abcdcs/abcdcs/msnd.py
@@ -2,12 +2,265 @@
 """Calculate MSND from PIV displacement fields for a single movie.
 """
 
+from __future__ import annotations
+
 from typing import Tuple
+import numpy as np
 import pandas as pd
 import xarray as xr
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+class MSND:
+    """Calculate MSND from PIV displacement fields for a single movie.
+    
+    Parameters
+    ----------
+    data : xr.Dataset | tuple[xr.DataArray, xr.DataArray]
+        PIV displacement fields. If a Dataset, must have the
+        components specified in `components`. The underlying DataArray
+        should have its dimensions including ('lag', 'T', 'Y', 'X') and 
+        dimension coordinates including lag, lag_s, T, T_s, Y, X.
+    components : tuple[str, str] | None, default = None
+        Components of the displacement fields. If `data` is a
+        Dataset, this must be specified. Otherwise, this is ignored
+    mpp : float | None, default = None
+        Physical pixel size in microns. If not specified, will try to 
+        get from `data` if it is a Dataset. If not found, will use pixel
+        as unit (same as specifying `mpp=1`).
+    fps : float | None, default = None
+        Frame rate in frames per second. If not specified, will
+        use coordinate variables found in `data`.
+    """
+
+    def __init__(
+        self, 
+        data: xr.Dataset | tuple[xr.DataArray, xr.DataArray],
+        components: tuple[str, str] | None = None,
+        mpp: float | None = None, 
+        fps: float | None = None,
+    ) -> None:
+        # prepare displacement fields
+        if isinstance(data, xr.Dataset):
+            if components is None:
+                raise ValueError(
+                    "components must be specified if data is a Dataset."
+                )
+            self._u, self._v = data[components[0]], data[components[1]]
+        else:
+            self._u, self._v = data
+        self._D2 = (self._u**2 + self._v**2).rename('D2')
+
+        
+        # use mpp specified. if not specified, try to get from piv 
+        # dataset. if not found, use pixel as unit.
+        if mpp is not None:
+            self._mpp = mpp
+        elif isinstance(data, xr.Dataset):
+            try:
+                self._mpp = data.attrs['pixelsize']
+            except KeyError:
+                self._use_pixel_as_unit()
+        else:
+            self._use_pixel_as_unit()
+
+        
+        # use fps specified. if not specified, leave it as None so 
+        # will use coords found in data.
+        self._fps = fps
+
+
+    def _use_pixel_as_unit(self):
+        self._mpp = 1.0
+        print("Cannot find physical pixel size. "
+              "Unit for MSND is now in pixel squared.")
+
+    def calculate(
+        self, 
+        method: str = 'normal', 
+        **kwargs
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        """Calculate MSND from PIV displacement fields.
+
+        Parameters
+        ----------
+        method : str, default = 'normal'
+            Method used to calculate MSND. Must be one of 'normal',
+            'weighted', 'eachlevel'.
+        **kwargs
+            Additional keyword arguments passed to the method specified
+            in `method`.
+
+            
+        Other parameters
+        ----------------
+        ### normal
+        no additional parameters are needed.
+
+        ### weighted
+        weights : xr.DataArray
+            Weights for each displacement field. Must have the same
+            dimensions as `data`. NaNs will be replaced with 0. So
+            don't rely on NaNs for magical masking side effects.
+            Perform masking to `data` explicitly before passing it
+            to this class.
+        
+        ### eachlevel
+        byimage : xr.DataArray
+            Array used to group the displacement fields. Must only 
+            dimensions that can be found from `data`.
+        bins : int | nd.ndarray
+            Bins to be passed to `np.digitize`.
+
+            
+        Returns
+        -------
+        stats : pd.DataFrame
+            MSND statistics. The columns are 'lag', 'lag_s', 'mean',
+            'std', 'sem', 'sizeT'. MSND quantities have unit in micron 
+            squared. Additional column 'level' is added if 
+            `method = 'eachlevel'`.
+        raw : pd.DataFrame
+            MSND calculated from individual PIV displacement fields. The
+            columns are 'lag', 'lag_s', 'T', 'T_s', 'msnd'. Additional 
+            column 'level' is added if `method = 'eachlevel'`. The extra 
+            columns 'T' and 'T_s' are for each time-sample. MSND's unit 
+            is in micron squared.
+        """
+        # dispatch to the method specified
+        self._dispatch(method=method)
+
+        # calculate msnd and convert to dataframe with non-NaN rows
+        da = self._calculate(**kwargs)
+        df = (da
+            .to_dataframe()
+            .dropna(axis=0, how='any')
+            .reset_index()
+            [self._colnames]
+        )
+
+        # apply unit conversion
+        df['msnd'] = df['msnd'] * self._mpp**2
+        if self._fps is not None:
+            df['lag_s'] = df['lag'] / self._fps
+            df['T_s'] = df['T'] / self._fps
+            print(f"Overwriting lag_s (and T_s) using fps = {self._fps} "
+                  f"({1000 / self._fps} ms between adjacent frames.)")
+            
+        # calculate statistics
+        stats = self._calculate_stats(df)
+            
+        return stats, df
+    
+
+    def _dispatch(self, method: str) -> None:
+        if method == 'normal':
+            self._colnames = ['lag', 'lag_s', 'T', 'T_s', 'msnd']
+            self._calculate = self._normal_msnd
+        elif method == 'weighted':
+            self._colnames = ['lag', 'lag_s', 'T', 'T_s', 'msnd']
+            self._calculate = self._weighted_msnd
+        elif method == 'eachlevel':
+            self._colnames = ['level', 'lag', 'lag_s', 'T', 'T_s', 'msnd']
+            self._calculate = self._eachlevel_msnd
+        else:
+            raise ValueError(
+                f"Unknown method: {method}. "
+                f"Must be one of 'normal', 'weighted', 'eachlevel'."
+            )
+
+    @staticmethod
+    def _msnd(D2: xr.DataArray) -> xr.DataArray:
+        return D2.mean(dim=('X', 'Y'), skipna=True).rename('msnd')
+
+    def _normal_msnd(self) -> xr.DataArray:
+        """Dispatched msnd calculation function for `method = 'normal'`
+        """
+        return self._msnd(self._D2)
+    
+    def _weighted_msnd(self, 
+        weights: xr.DataArray
+    ) -> xr.DataArray:
+        """Dispatched msnd calculation function for `method = 'weighted'`
+        """
+        weights = weights.fillna(0)
+        D2_weighted = (self._D2).weighted(weights)
+        return self._msnd(D2_weighted)
+    
+    def _eachlevel_msnd(self, 
+        byimage: xr.DataArray, 
+        bins: int | np.ndarray, 
+    ) -> xr.DataArray:
+        """Dispatched msnd calculation function for `method = 'eachlevel'`
+        """
+        # aligned image to displacement field
+        D2, image = xr.align(self._D2.rename('D2'), 
+                             byimage.rename('image'), 
+                             join='left')
+        # digitize image into levels
+        digitized = np.digitize(image, bins=bins, right=True)
+        # use right of each bin as level
+        # so for x in level e: bins[i-1] < x <= bins[i]=e
+        binlabels = bins[1:]
+
+        # calculate msnd for each level
+        msnd = []
+        for i,b in enumerate(binlabels):
+            msnd.append(
+                self._msnd(D2.where(digitized == i))
+                .assign_coords(level=b)
+            )
+
+        return xr.concat(msnd, dim='level')
+
+    def _calculate_stats(self, df: pd.DataFrame) -> pd.DataFrame:
+        groupby_cols = [c for c in self._colnames if c not in ['T', 'T_s', 'msnd']]
+        stats = (df
+            .groupby(groupby_cols)['msnd']
+            .aggregate(mean='mean', std='std', size='count')
+            .reset_index()
+        )
+        stats['sem'] = stats['std'] / np.sqrt(stats['size'])
+        return stats
+        
+
+
+
+
+def plot_msnd_stats(df, ax=None, errorbar=True, **kwargs):
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(4,3))
+    if errorbar:
+        _plot_msnd_stats(ax, df['lag_s'], df['mean'], df['std']/np.sqrt(df['size']), **kwargs)
+    else:
+        _plot_msnd_stats(ax, df['lag_s'], df['mean'], **kwargs)
+    try:
+        fig.set_tight_layout(True)
+    except UnboundLocalError:
+        pass
+
+    return ax
+
+def _plot_msnd_stats(ax, x, y, err=None, **kwargs):
+    if err is not None:
+        ax.errorbar(x=x, y=y, yerr=err, fmt='o', alpha=0.5, **kwargs)
+    else:
+        ax.scatter(x=x, y=y, alpha=0.5, **kwargs)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlabel('Lag time (s)')
+    ax.set_ylabel('MSND ($\mu m^2$/s)')
+
+    return ax
+
+
+def plot_msnd_raw(df):
+    g = sns.relplot(data=df, 
+                    x="T_s", y="msnd", hue="lag_s", 
+                    kind="line", palette="Set2")
+    return g
+    
 
 def calculate_msnd(
         ds, 
@@ -15,7 +268,7 @@ def calculate_msnd(
         weight=None, 
         pixsize=None
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    """MSND analysis from PIV displacement fields.
+    """(TO DEPRECATE) MSND analysis from PIV displacement fields.
 
     Compute MSND for each lag time with all possible pairs, but outliers
     (absolute value of zscore >= 3) excluded.
@@ -94,38 +347,3 @@ def calculate_msnd(
     )
     
     return stats, df
-
-
-def plot_msnd_stats(df, ax=None, errorbar=True, **kwargs):
-    if ax is None:
-        fig, ax = plt.subplots(figsize=(4,3))
-    if errorbar:
-        _plot_msnd_stats(ax, df['lag_s'], df['msnd_mean'], df['msnd_std'], **kwargs)
-    else:
-        _plot_msnd_stats(ax, df['lag_s'], df['msnd_mean'], **kwargs)
-    try:
-        fig.set_tight_layout(True)
-    except UnboundLocalError:
-        pass
-
-    return ax
-
-def _plot_msnd_stats(ax, x, y, err=None, **kwargs):
-    if err is not None:
-        ax.errorbar(x=x, y=y, yerr=err, fmt='o', alpha=0.5, **kwargs)
-    else:
-        ax.scatter(x=x, y=y, alpha=0.5, **kwargs)
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_xlabel('Lag time (s)')
-    ax.set_ylabel('MSND ($\mu m^2$/s)')
-
-    return ax
-
-
-def plot_msnd_raw(df):
-    g = sns.relplot(data=df[df["zscore"].abs()<3], 
-                    x="T_s", y="msnd", hue="lag_s", 
-                    kind="line", palette="Set2")
-    return g
-    
